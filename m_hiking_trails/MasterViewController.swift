3e361d679602781e6c5fc192d695b129c0d8e0e4
Ryan Evans <revans17@masonlive.gmu.edu>
2018-11-12 21:10:47 -0500

    update app

--- m_hiking_trails/MasterViewController.swift
+++ m_hiking_trails/MasterViewController.swift
@@ -11,20 +11,52 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
+    var hiking_trails_array = [hiking_trails]()
+    
 
+    func populateHikingTrailsArray() {
+        //First build URL for end-point
+        let endPoint = "http://www.protogic.com/universityservice/service.svc/allhikingtrails"
+        let jsURL:URL = URL(string: endPoint)!
+        
+        //Next, we will esxecute the end point; resulting in getting json
+        let jsonUrlData = try? Data(contentsOf: jsURL)
+        print(jsonUrlData ?? "Error: No data to print")
+        
+        //Consume JSON data
+        //Take JSON and Serialize it into a Dictionary
+        if jsonUrlData != nil {
+            let dictionary:NSDictionary = (try! JSONSerialization.jsonObject(with: jsonUrlData!, options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
+            print(dictionary)
+            let HTD = dictionary["HikingTrails"]! as! [[String:AnyObject]]
+            
+            for index in 0...HTD.count - 1 {
+                let singleTrail = HTD[index]
+                let tr = hiking_trails()
+                tr.TrailDifficulty = singleTrail["TrailDifficulty"] as! String
+                tr.TrailElevation = singleTrail["TrailElevation"] as! String
+                tr.TrailName = singleTrail["TrailName"] as! String
+                tr.TrailLength = singleTrail["TrailLength"] as! String
+                tr.TrailImage = singleTrail["TrailImage"] as! String
+                tr.TrailTime = singleTrail["TrailTime"] as! String
+                tr.TrailWebsite = singleTrail["TrailWebsite"] as! String
+                hiking_trails_array.append(tr)
+            }
+            
+        }
+        //Take Dictionary and iterate through values and add to hiking trails array
 
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
+        populateHikingTrailsArray()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+
+        
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -32,21 +64,14 @@ class MasterViewController: UITableViewController {
         super.viewWillAppear(animated)
     }
 
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
-
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let selectedTrail = hiking_trails_array[indexPath.row] as! hiking_trails
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = selectedTrail
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -60,31 +85,23 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return hiking_trails_array.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let selectedTrail = hiking_trails_array[indexPath.row] as! hiking_trails
+        cell.textLabel!.text = selectedTrail.TrailName
+        
+        cell.detailTextLabel!.text = selectedTrail.TrailDifficulty
+        
         return cell
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
 
 }
-
---
+++ m_hiking_trails/hiking_trails.swift
@@ -0,0 +1,22 @@
+//
+//  hiking_trails.swift
+//  m_hiking_trails
+//
+//  Created by Ryan Evans on 11/5/18.
+//  Copyright © 2018 Ryan Evans. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class hiking_trails {
+    var TrailDifficulty:String = ""
+    var TrailElevation:String = ""
+    var TrailID:Int = 0
+    var TrailImage:String = ""
+    var TrailLength:String = ""
+    var TrailName:String = ""
+    var TrailTime:String = ""
+    var TrailWebsite:String = ""
+    
+}
